ddf18475b54df4f2b8d96f1ae1c0931418015885
patricia-oliveira <patriciia.mota@gmail.com>
2022-03-15 19:32:54 +0000

    Add compareDNA method on pAequorFactory

--- javascript-first-part/mysterious-organism/main.js
+++ javascript-first-part/mysterious-organism/main.js
@@ -13,6 +13,8 @@ const mockUpStrand = () => {
     return newStrand
 }
 
+const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
 
 // Create object factory multiple for the organism
 const pAequorFactory = (specimenNum, dna) => {
@@ -23,6 +25,11 @@ const pAequorFactory = (specimenNum, dna) => {
             const mutationPosition = Math.floor(Math.random() * 15);
             const mutationBase = returnRandBase();
             this.dna[mutationPosition] = mutationBase;
+        },
+        compareDNA(otherSpecimenDNA) {
+            const differentDNABases = this.dna.filter(base => { return !otherSpecimenDNA.includes(base) });
+            const percentageCommonDNA = Math.floor(((this.dna.length - differentDNABases.length) / this.dna.length) * 100);
+            return `specimen #1 and specimen #2 have ${percentageCommonDNA}% DNA in common`;
         }
     }
 }
@@ -38,5 +45,20 @@ console.log("The second specimun should have identifier as 2 -> ", specimenNumTw
 console.log("The DNA strand length of first specimum should be 15 bases -> ", specimenNumOne.dna.length === 15)
 console.log("The DNA strand length of second specimum should be 15 bases -> ", specimenNumTwo.dna.length === 15)
 
+// Testing Space for mutate() method behavior
 specimenNumOne.mutate()
 console.log("The DNA strand length of first specimum mutated should be 15 bases -> ", specimenNumOne.dna.length === 15);
+
+
+// Testing Space for compareDNA() method behavior
+const specimenNumThree = pAequorFactory(3, ['A', 'G', 'G', 'G', 'T', 'T', 'C', 'C', 'C', 'G', 'G', 'T', 'T', 'C', 'G'])
+let specimenDNA = ['A', 'G', 'G', 'G', 'T', 'T', 'C', 'C', 'C', 'G', 'G', 'T', 'T', 'C', 'G'];
+let actualDNA = specimenNumThree.compareDNA(specimenDNA);
+let expectedComparison = "specimen #1 and specimen #2 have 100% DNA in common";
+console.log("All bases are identical on the same location, so should returns 100% of DNA in common ->", actualDNA === expectedComparison);
+
+
+specimenDNA = ['T', 'G', 'G', 'G', 'T', 'T', 'C', 'C', 'C', 'G', 'G', 'T', 'T', 'C', 'G'];
+actualDNA = specimenNumThree.compareDNA(specimenDNA);
+expectedComparison = "specimen #1 and specimen #2 have 93% DNA in common";
+console.log("Should returns 93% of DNA in common ->", actualDNA === expectedComparison);
