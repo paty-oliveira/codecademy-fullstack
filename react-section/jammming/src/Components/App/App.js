302f57d13189e0d1fc783e8966b637948db44dc7
patricia-oliveira <patriciia.mota@gmail.com>
2022-06-06 17:57:29 +0100

    Implement feature passing down playlist to tracklist

--- react-section/jammming/src/Components/App/App.js
+++ react-section/jammming/src/Components/App/App.js
@@ -2,15 +2,23 @@ import "./App.css";
 import React from "react";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchResults } from "../SearchResults/SearchResults";
+import { Playlist } from "../Playlist/Playlist";
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = { searchResults: [
-      {name: "Tiny Dancer", artist: "Elton John", album: "Madman Across The Water", id: 1},
-      {name: "Tiny Dancer", artist: "Tim McGraw", album: "Love Story", id: 2},
-      {name: "Tiny Dancer", artist: "The White Raven", album: "Tiny Dancer", id: 3}
-    ]
+    this.state = {
+      searchResults: [
+        {name: "Tiny Dancer", artist: "Elton John", album: "Madman Across The Water", id: 1},
+        {name: "Tiny Dancer", artist: "Tim McGraw", album: "Love Story", id: 2},
+        {name: "Tiny Dancer", artist: "The White Raven", album: "Tiny Dancer", id: 3}
+      ],
+      playlistName: "Playlist by Patricia",
+      playlistTracks: [
+        {name: "Stronger", artist: "Britney Spears", album: "Oops!... I Did it Again", id: 4},
+        {name: "So Emotional", artist: "Whitney Houston", album: "Whitney", id: 5},
+        {name: "It's Not Right But It's Okay", artist: "Whitney Houston", album: "My Love is Your Love", id: 6}
+      ]
   };
 }
 
@@ -23,7 +31,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}/>
-            {/* Add a Playlist component */}
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
