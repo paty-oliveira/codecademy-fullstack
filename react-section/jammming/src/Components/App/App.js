dc662eceae18996f20d6764217fc16659fa9b5b8
patricia-oliveira <patriciia.mota@gmail.com>
2022-06-07 17:08:42 +0100

    Implement feature of removing tracks to a playlist

--- react-section/jammming/src/Components/App/App.js
+++ react-section/jammming/src/Components/App/App.js
@@ -21,15 +21,23 @@ class App extends React.Component {
       ]
   };
   this.addTrack = this.addTrack.bind(this);
+  this.removeTrack = this.removeTrack.bind(this);
 }
 
   addTrack(track) {
     let tracks = this.state.playlistTracks;
-    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
-      return;
+    if (!tracks.find(savedTrack => savedTrack.id === track.id)) {
+      tracks.push(track);
     }
-    tracks.push(track);
+
     this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks} );
   }
 
   render() {
@@ -40,7 +48,8 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}/>
           </div>
         </div>
       </div>
--- react-section/jammming/src/Components/TrackList/TrackList.js
+++ react-section/jammming/src/Components/TrackList/TrackList.js
@@ -9,7 +9,9 @@ export class TrackList extends React.Component {
             track => {
                 return <Track track={track}
                     key={track.id}
-                    onAdd={this.props.onAdd} />
+                    onAdd={this.props.onAdd}
+                    onRemove={this.props.onRemove}
+                    isRemoval={this.props.isRemoval}/>
             }
         );
     }
