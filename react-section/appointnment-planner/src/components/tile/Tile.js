ec1839d7d8e795e12388a177aed18f240456a83b
patricia-oliveira <patriciia.mota@gmail.com>
2022-07-18 10:28:55 +0100

    Add implementation for Title component

--- react-section/appointnment-planner/src/components/tile/Tile.js
+++ react-section/appointnment-planner/src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
+
+  const contactAttr = Object.values(props.contact);
+
   return (
     <div className="tile-container">
-      
+      {contactAttr.map((attr, index) => {
+        return index === 0 ?
+          <p className="tile-title" key={index}>{attr}</p> :
+          <p className="tile" key={index}>{attr}</p>
+      })}
     </div>
   );
 };
--- react-section/appointnment-planner/src/components/tileList/TileList.js
+++ react-section/appointnment-planner/src/components/tileList/TileList.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const TileList = () => {
+import { Tile } from '../tile/Tile';
+
+export const TileList = (props) => {
+
+  const contacts = props.contacts;
+
   return (
     <div>
-      
+      {contacts.map((contact, index) => {
+        return <Tile contact={contact} key={index} />
+      })}
     </div>
   );
 };
