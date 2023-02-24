e0d97cdf5545f973ef2ab90473228c956c7873e2
patricia-oliveira <patriciia.mota@gmail.com>
2023-02-24 09:53:39 +0000

    Finish OAuth implementation

--- express-section/github-auth/app.js
+++ express-section/github-auth/app.js
@@ -24,13 +24,25 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
  * Passport Configurations
 */
 
-passport.use(new GitHubStrategy({
-  clientId: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/github/callback'
-}), function(accessToken, refreshToken, profile, done){
-  return done(null, profile);
-})
+passport.use(new GitHubStrategy(
+{
+          clientId: GITHUB_CLIENT_ID,
+          clientSecret: GITHUB_CLIENT_SECRET,
+          callbackURL: 'http://localhost:3000/auth/github/callback'
+    }),
+    (accessToken, refreshToken, profile, done) => {
+        return done(null, profile);
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+
+passport.deserializeUser((user,done) => {
+  done(null, user);
+});
+
 
 /*
  *  Express Project Setup
@@ -57,7 +69,21 @@ app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/auth/github',
+    passport.authenticate('github',  {
+      scope: ["user"]
+    })
+);
+
+app.get('/auth/github/callback',
+    passport.authenticate('github', {
+      failureRedirect: '/login',
+      successRedirect: '/'
+    })
+);
+
+
+app.get('/account', ensureAuthenticated, (req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -80,3 +106,10 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 /*
  * ensureAuthenticated Callback Function
 */
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect('/login');
+}
