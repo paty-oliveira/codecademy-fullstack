6b16e2ba81b5c0c76a705a2efcccfdbfdd57ce30
patricia-oliveira <patriciia.mota@gmail.com>
2023-04-04 11:50:45 +0100

    Implement updateBook function

--- api-development/book-store/frontend/src/api/books.js
+++ api-development/book-store/frontend/src/api/books.js
@@ -22,6 +22,22 @@ export const getBooks = async () => {
 	return await response.json();
 };
 
-// TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
+export const updateBook = async (id, newTitle, newStart, newEnd) => {
+	const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+		method: "PUT",
+		body: JSON.stringify({
+			newTitle,
+			newStart,
+			newEnd
+		}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	});
+
+	return response.status;
+};
+
+
 
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
