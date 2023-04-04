cb90dc257134fae7ed6b3edee78eccc5c84a6634
patricia-oliveira <patriciia.mota@gmail.com>
2023-04-04 11:53:31 +0100

    Implement deleteBook function

--- api-development/book-store/frontend/src/api/books.js
+++ api-development/book-store/frontend/src/api/books.js
@@ -38,6 +38,10 @@ export const updateBook = async (id, newTitle, newStart, newEnd) => {
 	return response.status;
 };
 
+export const deleteBook = async (id) => {
+	const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+		method: "DELETE"
+	});
 
-
-// TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
+	return response.status;
+};
