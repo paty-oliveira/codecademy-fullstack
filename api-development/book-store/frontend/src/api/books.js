14fa101d527a263d55b28a79b5cb0fae1b04589d
patricia-oliveira <patriciia.mota@gmail.com>
2023-04-04 11:43:29 +0100

    Add addNewBook function

--- api-development/book-store/frontend/src/api/books.js
+++ api-development/book-store/frontend/src/api/books.js
@@ -1,6 +1,20 @@
-// TODO: Import API_ENDPOINT
+import {API_ENDPOINT} from "./index.js";
+export const addNewBook = async (newTitle, newStart, newEnd) => {
+	const response = await fetch(`${API_ENDPOINT}/books`, {
+		method: "POST",
+		body: JSON.stringify({
+			title: newTitle,
+			start: newStart,
+			end: newEnd
+		}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	});
 
-// TODO: Create the addNewBook function that takes in newTitle, newStart, and newEnd as arguments. Inside the function, create a POST request for the new book. Store the response as a JSON in a variable called newBook and return it at the end of the function.
+	return await response.json();
+};
+
 
 // TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
 
